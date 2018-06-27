c6b69a3603986e377dae509fe41523c6a3867a43
drumath <2237k.drumath@gmail.com>
2018-06-28 00:36:09 +0900

    ppm_out()を実装したが、使用が迷子になっているので、確定させたい

--- image.h
+++ image.h
@@ -2,18 +2,37 @@
 #define H_IMAGE
 
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+
+#include "vec.h"
 
 using namespace std;
 
 struct PPM
 {
-  int width;
-  int height;
-  string file_name;
-  int max_color;
+  int width=600;
+  int height=400;
+  int max_color=255;
+  string file_name = "result.ppm";
+  vector<Vec> C;
 
-  PPM():width(600),height(400),file_name("result.ppm"),max_color(255){}
+  PPM(){}
+  PPM(int w, int h):width(w),height(h){}
+  PPM(string f):file_name(f){}
 };
+
+void ppm_out(PPM* ppm)
+{
+  ofstream ofs(ppm->file_name);
+  ofs << "P3\n" << ppm->width << " " << ppm->height << "\n" << ppm->max_color << endl;
+
+  for(int i=0; i<ppm->width*ppm->height; i++){
+    ofs << ppm->C[i].x << " "
+        << ppm->C[i].y << " "
+        << ppm->C[i].z;
+  }
+}
 
 #endif
