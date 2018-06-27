87becff1665d90b10ace7ef671f3674954e066c4
drumath <2237k.drumath@gmail.com>
2018-06-27 23:59:10 +0900

    ppm構造体を作ってimage構造体から継承しないことにした。メンバは４つ。あとでOneNoteにまとめる

--- image.h
+++ image.h
@@ -2,11 +2,18 @@
 #define H_IMAGE
 
 #include <iostream>
+#include <string>
 
-struct Image
+using namespace std;
+
+struct PPM
 {
   int width;
   int height;
+  string file_name;
+  int max_color;
+
+  PPM():width(600),height(400),file_name("result.ppm"),max_color(255){}
 };
 
 #endif
