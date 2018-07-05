339c9b5de1a490ec1e684be87bcd5b1cde940705
drumath <2237k.drumath@gmail.com>
2018-07-06 00:18:12 +0900

    scene.hの名前のスペル間違えてたw そしてintersect()を実装しようとした

--- scene.h
+++ scene.h
@@ -5,9 +5,15 @@
 #include <optional>
 
 #include "sphere.h"
+#include "hit.h"
 
 struct Scene {
   std::vector<Sphere> spheres;
+
+  std::optional<Hit> intersect(Ray& ray, double tmin, double tmax)
+  {
+    
+  }
 };
 
 #endif // !H_SCENE
