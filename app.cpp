54d9fd632e6cc1c80399e05e886eb75ac7bb7038
drumath <2237k.drumath@gmail.com>
2018-07-26 16:03:03 +0900

    main()が正常に動いていることが確認できた。

--- app.cpp
+++ app.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 #include "dript.h"
 
 using namespace std;
 
-main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
-  
+  int w = 1200;
+  int h = 800;
+
+  ofstream ofs("result.ppm");
+  ofs << "P3\n" << w << " " << h << "\n255\n";
+  for(int i=0; i<w*h; i++){
+    ofs << "255 0 255" << endl;
+  }
   return 0;
 }
