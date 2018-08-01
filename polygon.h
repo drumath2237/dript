b870e7f11eac81e9ca7393ec0c090a00657b75a9
drumath <2237k.drumath@gmail.com>
2018-08-01 22:17:39 +0900

    polygonの仕様を変更し、法線を返すメソッドを実装した。

--- polygon.h
+++ polygon.h
@@ -11,29 +11,18 @@
 using namespace std;
 
 struct Polygon3 {
-  vector<Vec> vertexes = {
-    {}, {}, {}
-  };
+  Vec a, b, c;
+  
+  Polygon3(vector<Vec> vs):a(vs[0]),b(vs[1]),c(vs[2]){}
 
-  Polygon3(vector<Vec> vs)
-  {
-    if(vs.size() == 3){
-      for(int i=0; i<3; i++){
-        vertexes[i] = vs[i];
-      }
-    }
-  }
-
-  Polygon3(Vec a, Vec b, Vec c)
-  {
-    vertexes[0] = a;
-    vertexes[1] = b;
-    vertexes[2] = c;
-  }
+  Polygon3(Vec a, Vec b, Vec c):a(a),b(b),c(c){}
 
   Vec normal_vec()
   {
-    // TODO: 法線を返す関数の実装
+    Vec AB = b - a;
+    Vec BC = c - b;
+
+    return normalize(cross(AB, BC));
   }
 
   optional<Hit> intersect(Ray ray, double tmin, double tmax)
