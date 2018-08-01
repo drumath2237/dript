e18efa0f518e70503bfbb195e5c19b435711fc5f
drumath <2237k.drumath@gmail.com>
2018-08-01 20:09:37 +0900

    vec構造体に外積を導入した

--- vec.h
+++ vec.h
@@ -53,5 +53,14 @@ Vec normalize(Vec v)
   return v/length(v);
 }
 
+Vec cross(Vec a, Vec b)
+{
+  return Vec(
+    a.y*b.z - a.z*b.y,
+    a.z*b.x - a.x*b.z,
+    a.x*b.y - a.y*b.x
+  );
+}
+
 
 #endif // !H_VEC
